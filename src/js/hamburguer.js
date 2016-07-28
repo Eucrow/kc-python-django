43caf97a09b88c11c35c72f2512b8d7307d17850
Marco A. Amez <marco.a.amez@gmail.com>
2016-07-28 20:57:46 +0200

    Create header, footer and mobile menu

---
+++ src/js/hamburguer.js
@@ -0,0 +1,21 @@
+var $ =require('jquery');
+
+//adaptado de: https://codepen.io/g13nn/pen/eHGEF
+
+$( ".cross" ).hide();
+
+$( ".menu" ).hide();
+
+$( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "150", function() {
+        $( ".hamburger" ).hide();
+        $( ".cross" ).show();
+        });
+});
+
+$( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "250", function() {
+        $( ".cross" ).hide();
+        $( ".hamburger" ).show();
+    });
+});
