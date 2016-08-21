b16c46111e52b8c8f31ce77a17c6f74d8350f44a
Marco A. Amez <marco.a.amez@gmail.com>
2016-08-21 13:46:28 +0200

    Correct time counter in the case of elapsed time is less than a week

--- src/js/datetime.js
+++ src/js/datetime.js
@@ -24,47 +24,49 @@ moment.updateLocale('es', {
     var times =  $(".container-article").find("time");
     for (var i=0; i<times.length; i++) {
         time = $(times[i]);
-        refreshTime(time);
+        refresh(time);
     }
 
-    function refreshTime(time){
-        comment_time = moment($(time).attr("datetime"), "YYYY-MM-DD HH:mm:ss");
-        now_time = moment();
-        elapsed_time = now_time.diff(comment_time, 'seconds'); // tiempo que hay
+    function refresh(time){
+        publicationTime = moment($(time).attr("datetime"), "YYYY-MM-DD HH:mm:ss");
+        nowTime = moment();
+        timeToNow = nowTime.diff(publicationTime, 'seconds'); // tiempo que hay
         // entre que se publica el artículo hasta el momento en que se refresca la pantalla
 
         // Función para actualizar y refrescar el tiempo transcurrido
         // Sólo vale para algunos casos... :(
         function updateAndRefresh(timeToRefresh){
-            $(time).text(comment_time.fromNow());
+            $(time).text(publicationTime.fromNow());
             setTimeout(datetime, timeToRefresh);
         }
 
-        // Según el tiempo que ha pasado desde la publicación
-        if (elapsed_time < 60){ // menos de un minuto
-            updateAndRefresh(1000); // refresca cada segundo
-        } else if (elapsed_time < (60*60)) { // menos de una hora
-            updateAndRefresh(60000); // refresca cada minuto
-        } else if (elapsed_time < (60*60*24)){ // menos de un día
-            // En el caso de las horas, mejor refrescar cuando ha pasado una hora desde
-            // la publicación:
-                minuto_pub =  moment(comment_time).minutes();
-                minuto_act =  moment(now_time).minutes();
+        // According to the time elapsed from the publication of the article
+        if (timeToNow < 60){ // less than a minute
+            updateAndRefresh(1000); // refresh every second
+        } else if (timeToNow < (60*60)) { // less than a hour
+            updateAndRefresh(60000); // refresh every minute
+        } else if (timeToNow < (60*60*24)){ // less than a day:
+            // In case of days, better if refresh at 1 hour from the publication:
+                minutePublication =  moment(publicationTime).minutes();
+                minuteNow =  moment(nowTime).minutes();
                 timeToRefresh = 0;
-
-                if (minuto_pub <= minuto_act) {
-                    timeToRefresh = 60 - (minuto_act - minuto_pub);
-                } else if (minuto_pub > minuto_act) {
-                    timeToRefresh = minuto_pub - minuto_act;
+                if (minutePublication <= minuteNow) {
+                    timeToRefresh = 60 - (minuteNow - minutePublication);
+                } else if (minutePublication > minuteNow) {
+                    timeToRefresh = minutePublication - minuteNow;
                 }
-
             updateAndRefresh( timeToRefresh*60*1000 );
-        } else if (elapsed_time < (60*60*24*7)) { //menos de una semana
-            var to_append = "el " + moment(comment_time).format('dddd');
+        } else if (timeToNow < (60*60*24*7)) { // less than a week
+            // In case of weeks, refresh with the change of the days.
+                // Next day (add(1,'d')) at 0 hours and 0 minutes:
+                timeRefresh = moment().add(1, 'd').hours(0).minutes(0);
+                // milliseconds to the next day:
+                timeToRefresh = moment(timeRefresh).diff(moment());
+            var to_append = "el " + moment(publicationTime).format('dddd');
             $(time).text(to_append);
-            setTimeout(datetime, (24*60*60*1000)); // refresca cada día
-        } else if (elapsed_time > (60*60*24*7)) { //mas de una semana
-            $(time).text(comment_time.format('LLL'));
+            setTimeout(datetime, timeToRefresh);
+        } else if (timeToNow > (60*60*24*7)) { // more than a week
+            $(time).text(publicationTime.format('LLL'));
         }
 
     }
