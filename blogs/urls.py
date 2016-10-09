829e9ba23ffa99d3a9129eb42146fb11e3cef969
Marco A. Amez <marco.a.amez@gmail.com>
2016-10-09 13:30:31 +0200

    Correct global style

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,6 +2,6 @@ from blogs.views import Blog, BlogList
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^blogs/(?P<user_blog>.+)$', Blog.as_view()),
+    url(r'^blogs/(?P<user_blog>.+).html$', Blog.as_view()),
     url(r'^blogs', BlogList.as_view()),
 ]
