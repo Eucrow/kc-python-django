36babca41ae2effca0b0ad488a7a22400bcb243c
Marco A. Amez <marco.a.amez@gmail.com>
2016-10-23 19:12:00 +0200

    Add new post -- doesn't work

---
+++ cuartium/static/src/js/post-creation-form.js
@@ -0,0 +1,53 @@
+/**
+ * Created by MarcoAntonio on 21/10/2016.
+ */
+var $ = require ('jquery');
+
+var moment = require('moment');
+
+// function to convert to YYYY-MM-DD format if it is in DD-MM-YYYY format
+function convert_date(date){
+    if (moment(date, "DD-MM-YYYY").isValid()){
+        var momentDate = moment(date, "DD-MM-YYYY");
+        correctDate = momentDate.format("YYYY-MM-DD");
+        return (correct_date);
+    } else if (moment(date, "YYYY-MM-DD").isValid()){
+        return (date);
+    } else {
+        return ("ERROR");
+    }
+}
+
+//event listener al botón de enviar formulario
+$('.new-post-form').on("submit", function(){
+    var inputs = $('.new-post-form').find("input, textarea, select");
+
+    for (var i= 0; i<inputs.length; i++){
+        var input = inputs[i];
+        if (input.checkValidity() == false){
+            alert(input.validationMessage);
+            input.focus();
+            return false;
+        }
+    }
+
+    // validate date
+    var dateField = $('#publication_date');
+    correct_date = convert_date(dateField.val());
+    alert(correct_date);
+    if (correct_date == "ERROR") {
+        //no se por qué la siguiente línea no funciona:
+        //date_field.setCustomValidity("fecha incorrecta");
+        alert ("fecha incorrecta")
+        input.focus();
+        return false;
+    } else {
+        // no se por qué la siguiente línea no funciona:
+        // $('#publication_date').val() = publication_date;
+        document.getElementById('publication_date').value = correct_date ;
+        alert(document.getElementById('publication_date').value);
+        alert("kk")
+    }
+
+
+})
