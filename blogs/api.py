bbbbbe1d0c838d8dcc95036fbd82d9deacdc9450
Marco A. Amez <marco.a.amez@gmail.com>
2016-11-04 17:48:55 +0100

    Correct API blogs

--- blogs/api.py
+++ blogs/api.py
@@ -9,5 +9,15 @@ class BlogListAPI(ListCreateAPIView):
     Endpoint de listado de blogs
     """
 
-    queryset = User.objects.all()
+    user_list = User.objects.all()
     serializer_class = BlogSerializer
+
+    def get_queryset(self):
+        # filter by search
+        search = self.request.query_params.get('search', None)
+
+        if search is not None:
+            queryset = self.user_list.filter(username__icontains=search)
+            return queryset
+        else:
+            return self.user_list
